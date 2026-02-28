f89f2d1ed472cbeb3f85ef12ffb72f1e746803f0
GitHub Action <action@github.com>
2026-02-28 09:43:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/jtdegraaf/jpql2sql/MyBundle.kt
+++ src/main/kotlin/com/github/jtdegraaf/jpql2sql/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jtdegraaf.jpql2sql
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jtdegraaf/jpql2sql/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/jtdegraaf/jpql2sql/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.jtdegraaf.jpql2sql.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
